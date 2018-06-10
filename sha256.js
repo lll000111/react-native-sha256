eb57a8574f478cd769f933b721d65b5246c95d22
Michael Hasenstein <hasenstein@yahoo.com>
2018-06-10 10:51:43 +0200

    add a JSDoc comment for better IDE help (popup info window for a function)

--- sha256.js
+++ sha256.js
@@ -4,6 +4,11 @@
 const {NativeModules} = require('react-native');
 const sha256Lib = NativeModules.sha256Lib;
 
+/**
+ * Calculate SHA-256 hash of a string. The hash is returned as lowercase hexadecimal string.
+ * @param {string} data
+ * @returns {Promise<string>}
+ */
 export function sha256 (data: string): Promise<string> {
     return sha256Lib.sha256(data);
 }

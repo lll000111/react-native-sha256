8b85cdd4c63e79d9781e209ee63ce7ae0210e62e
Michael Hasenstein <hasenstein@yahoo.com>
2018-06-10 10:45:29 +0200

    return type is Promise<string>, not string

--- sha256.js
+++ sha256.js
@@ -4,6 +4,6 @@
 const {NativeModules} = require('react-native');
 const sha256Lib = NativeModules.sha256Lib;
 
-export function sha256 (data: string): string {
+export function sha256 (data: string): Promise<string> {
     return sha256Lib.sha256(data);
 }
